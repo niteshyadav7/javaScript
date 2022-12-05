5fdf21539887f3a4087fd72ac665c373d3964e0c
niteshyadav7 <nitesh0709yadav@gmail.com>
2022-12-05 21:51:26 +0530

    notes on var and let

---
+++ prob01.js
@@ -0,0 +1,13 @@
+//   javaScript is Synco. and single thread language
+// var --> redeclare ho jata hai
+// jo ki nhi hona chahiye
+var num=12;
+var num=13;
+console.log(num );
+
+// let -->redeclare nhi hota hai
+//  jo a6a hota hai
+//SyntaxError: Identifier 'a' has already been declared
+let a=1222;
+let a=12;
+ccosnole.log(a);
